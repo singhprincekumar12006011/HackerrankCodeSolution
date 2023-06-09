3f5b8da3630583542350d5fecee8a6aaf5f41994
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-09 23:24:16 +0530

    Add files via upload

---
+++ java-if-else.java
@@ -0,0 +1,85 @@
+/*  
+
+In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes. An if-else statement has the following logical flow:
+
+Wikipedia if-else flow chart
+
+Source: Wikipedia
+
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation
+
+Sample Case 0: 
+ is odd and odd numbers are weird, so we print Weird.
+
+Sample Case 1: 
+ and  is even, so it isn't weird. Thus, we print Not Weird.
+
+*/
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        int n;
+        String ans="";
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        if(n % 2 == 1)
+        {
+            ans = "Weird";
+        } else {
+            if ( n >= 2 && n <= 5){
+                ans = "Not Weird";
+            } else if ( n >= 6 && n <= 20) {
+                ans = "Weird";
+            } else {
+                ans="Not Weird";
+            }
+        }
+        System.out.println(ans);
+    }
+}
