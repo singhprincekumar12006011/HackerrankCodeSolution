0898f44e7395184f25d69fc36c4cf248f1c532e5
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-10-22 21:51:21 +0530

    Create Java_End-of-file.java
    intial commit 
    
    java end-of-file

---
+++ Java_End-of-file.java
@@ -0,0 +1,58 @@
+/**
+
+The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
+
+Hint: Java's Scanner.hasNext() method is helpful for this problem.
+
+Input Format
+
+Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+Output Format
+
+For each line, print the line number, followed by a single space, and then the line content received as input.
+
+Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+Sample Output
+
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.
+
+**/
+
+
+Solution:- 
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+    Scanner scanner=new Scanner(System.in);
+    String inputLine;
+    int i=1;
+        while (scanner.hasNextLine()) {
+            inputLine = scanner.nextLine();
+            
+            
+            if (inputLine.isEmpty()) {
+                break; // Exit the loop when an empty line is entered
+            }
+            
+            System.out.println(i +" " + inputLine);
+            i++;
+        }
+        
+    }
+}
